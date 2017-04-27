4ed56d09a2753286362a44ffdf6dac0ca1315b60
frksptr <frkspeter@gmail.com>
2017-04-27 12:56:38 +0200

    korkereso.py

---
+++ korkereso.py
@@ -0,0 +1,20 @@
+import numpy as np
+r = 1;
+p1 = np.array([1, 0]).transpose();
+p2 = np.array([0, -1]).transpose();
+pi = np.pi;
+
+center = (p1+p2)/2;
+diff = p2-p1;
+
+a = np.linalg.norm(diff);
+m = np.sqrt(r*r-(a/2)*(a/2));
+mtot = r + m;
+
+alpha = np.arctan(diff[1]/diff[0]);
+beta = alpha+pi/2;
+
+beta = np.array([alpha+pi/2, -pi/2+alpha]).transpose();
+
+p3 =  np.column_stack((np.cos(beta),np.sin(beta),np.array([1, 1])))
+p3 = p3.dot( np.vstack((mtot * np.identity(2), center)) )
